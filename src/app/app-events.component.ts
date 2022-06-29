5b309b810bff6cb8ffaa8126df6ba832724cc139
Hugo Mata <hugo.da-mata@siemens-energy.com>
2022-06-29 17:24:50 -0300

    Bootstraping the Angular app. Hello World in the main page. App component renamed to app-events.

---
+++ src/app/app-events.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  template: '<h1>Hello World</h1>'
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
