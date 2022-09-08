75193a62a1e5dee5342e969fbf32f2e2ebb51431
Hugo Mata <hugo.da-mata@siemens-energy.com>
2022-09-08 08:49:24 -0300

    New changes following the course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './app-events.component';
 
 @NgModule({
-  declarations: [
-    EventsAppComponent
-  ],
   imports: [
+    // add the module here. Importing a module will make it available to the app.
+    // all of its exported declarations and providers will be available to the app.
     BrowserModule
   ],
-  providers: [],
+  declarations: [
+    // add the component, directive, pipe here
+    EventsAppComponent
+  ],
+  providers: [
+    // add the service here
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
